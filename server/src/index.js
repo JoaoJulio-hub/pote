2067c49ff1e2135059ce239f1d094e31a6bde2e7
João Júlio <78657127+JoaoJulio-hub@users.noreply.github.com>
2023-04-24 13:36:54 +0100

    new cors url

--- server/src/index.js
+++ server/src/index.js
@@ -19,12 +19,7 @@ app.use(cors())
 
 mongoose.connect(process.env.MONGODB_URL)
 
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-  },
-})
+const io = new Server(server)
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`)
